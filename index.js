a121d9dd180f4cc57c2efcff16ea88fa20f22124
storm <stormarasa2@gmail.com>
2023-10-12 20:22:32 +0300

    initial commit after declaring functions

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 /* Your Code Here */
+function hoursWorked(timeIn, timeOut){
+    const minutesIn = new Date(timeIn).getMinutes();
+    const minutesOut = new Date(timeOut).getMinutes();
+    const timeWorked = (new Date(timeOut) - new Date(timeIn))/ (1000*60*60)
+    const minutesInWork = (minutesIn / 60) + (minutesOut / 60)
+    return timeWorked - minutesInWork
+}
 
+function createEmployeeRecord(arr){
+    return{
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array){
+    return arrays.map(arr => createEmployeeRecord(arr))
+
+}
+
+function createTimeInEvent(calenderDay){
+    const [date, hour] = calenderDay.split('')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour),
+
+    })
+    return this;
+}
+
+function createTimeOutEvent(calenderDay){
+    const [date, hour] = calenderDay.split('')
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        date: date,
+        hour:parseInt(hour)
+    })
+    return this;
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
